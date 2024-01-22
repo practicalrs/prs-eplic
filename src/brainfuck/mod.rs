f8684edac6c4df2f9f0fce385516225173fd44af
Michal Piotrowski <practicalrs.com@gmail.com>
2024-01-22 22:36:08 +0100

    #2 Brainfuck interpreter p1
     #2 Brainfuck interpreter p2
    
     #2 Brainfuck interpreter p3
    
     #2 Brainfuck interpreter p4
    
     #2 Brainfuck interpreter p5
    
     #2 Brainfuck interpreter p6
    
     #2 Brainfuck interpreter p8
    
     #2 Brainfuck interpreter p9
    
     #2 Brainfuck interpreter p10
    
     #2 Brainfuck interpreter p11
    
     #2 Brainfuck interpreter p12
    
     #2 Brainfuck interpreter p13
    
     #2 Brainfuck interpreter p14
    
     #2 Brainfuck interpreter p15
    
     #2 Brainfuck interpreter p16
    
     #2 Brainfuck interpreter p17
    
     #2 Brainfuck interpreter p18
    
     #2 Brainfuck interpreter p19
    
     #2 Brainfuck interpreter p20
    
     #2 Brainfuck interpreter p21

---
+++ src/brainfuck/mod.rs
@@ -0,0 +1,134 @@
+/// Heavily inspired by:
+/// https://github.com/nushell/nushell/blob/main/crates/nu-parser/src/lex.rs
+/// https://github.com/Overv/bf/blob/master/src/main.rs
+use crate::Result;
+use std::io::{stdin, Read};
+
+#[derive(Debug)]
+enum Cmd {
+    Dec,
+    DecPtr,
+    Inc,
+    IncPtr,
+    Lp(Vec<Cmd>),
+    LpBg,
+    LpEn,
+    Ld,
+    St,
+}
+
+fn exec(cmds: &[Cmd], mem: &mut [u8], ptr: &mut usize) -> Result<()> {
+    for cmd in cmds {
+        match cmd {
+            Cmd::Dec => mem[*ptr] -= 1,
+            Cmd::DecPtr => *ptr -= 1,
+            Cmd::Inc => mem[*ptr] += 1,
+            Cmd::IncPtr => *ptr += 1,
+            Cmd::Ld => print!("{}", mem[*ptr] as char),
+            Cmd::Lp(cmds) => {
+                while mem[*ptr] != 0 {
+                    exec(cmds, mem, ptr)?
+                }
+            }
+            Cmd::St => {
+                let mut inpt = [0; 1];
+                stdin().read_exact(&mut inpt)?;
+                mem[*ptr] = inpt[0];
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
+
+fn lex(inpt: &[u8]) -> Vec<Cmd> {
+    let mut cmds = vec![];
+    let mut off = 0;
+
+    while let Some(c) = inpt.get(off) {
+        let cmd = match c {
+            b'>' => Some(Cmd::IncPtr),
+            b'<' => Some(Cmd::DecPtr),
+            b'+' => Some(Cmd::Inc),
+            b'-' => Some(Cmd::Dec),
+            b'.' => Some(Cmd::Ld),
+            b',' => Some(Cmd::St),
+            b'[' => Some(Cmd::LpBg),
+            b']' => Some(Cmd::LpEn),
+            _ => None,
+        };
+
+        if let Some(cmd) = cmd {
+            cmds.push(cmd);
+        }
+
+        off += 1;
+    }
+
+    cmds
+}
+
+fn parse(cmds: &[Cmd]) -> Vec<Cmd> {
+    let mut parsed_cmds = vec![];
+    let mut lp_bg = 0;
+    let mut lp_stck = 0;
+
+    for (i, cmd) in cmds.iter().enumerate() {
+        if lp_stck == 0 {
+            let parsed_cmd = match cmd {
+                Cmd::Dec => Some(Cmd::Dec),
+                Cmd::DecPtr => Some(Cmd::DecPtr),
+                Cmd::Inc => Some(Cmd::Inc),
+                Cmd::IncPtr => Some(Cmd::IncPtr),
+                Cmd::Ld => Some(Cmd::Ld),
+                Cmd::Lp(_) => None,
+                Cmd::LpBg => {
+                    lp_bg = i;
+                    lp_stck += 1;
+                    None
+                }
+                Cmd::LpEn => panic!("aaaa"),
+                Cmd::St => Some(Cmd::St),
+            };
+
+            if let Some(parsed_cmd) = parsed_cmd {
+                parsed_cmds.push(parsed_cmd);
+            }
+        } else {
+            match cmd {
+                Cmd::LpBg => {
+                    lp_stck += 1;
+                }
+                Cmd::LpEn => {
+                    lp_stck -= 1;
+
+                    if lp_stck == 0 {
+                        parsed_cmds.push(Cmd::Lp(parse(&cmds[lp_bg + 1..i])));
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    if lp_stck != 0 {
+        panic!("AAA");
+    }
+
+    parsed_cmds
+}
+
+pub fn run(inpt: &[u8]) -> Result<()> {
+    let cmds = lex(inpt);
+    let parsed_cmds = parse(&cmds);
+
+    let mut mem = vec![0; 1024];
+    let mut ptr = 512;
+
+    exec(&parsed_cmds, &mut mem, &mut ptr)?;
+
+    //println!("MEM = {mem:?}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("EPLIC");
+#![forbid(unsafe_code)]
+
+/// Please forgive me for using all these abbreviations all over the code.
+/// I just wanted to check how it feels to use Rust like my favorite
+/// macro assembler - C language ;)
+use std::{env, error::Error, fs::read};
+
+mod brainfuck;
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+enum Lng {
+    Brainfuck,
+    Unknown,
+}
+
+fn main() -> Result<()> {
+    println!("Practical RS Esoteric Programming Languages Interpreters Collection.");
+
+    let args: Vec<String> = env::args().collect();
+    let file = args.last().unwrap();
+    let ext = file.split('.').last();
+
+    let lng = if let Some(ext) = ext {
+        match ext {
+            "bf" => Lng::Brainfuck,
+            _ => Lng::Unknown,
+        }
+    } else {
+        Lng::Unknown
+    };
+
+    let inpt = read(file)?;
+
+    if let Lng::Brainfuck = lng {
+        let _ = brainfuck::run(&inpt);
+    }
+
+    Ok(())
 }
